7f3687210035eb73f77352cd7991e79c127a8883
Du <duyongquan@gmail.com>
2026-03-11 13:52:28 -0600

    chore: exclude large static assets from function tracing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    // Avoid bundling massive local image folders into serverless functions.
+    outputFileTracingExcludes: {
+      "*": [
+        "public/products/**/*",
+        "public/supplier-logos/**/*",
+        "tmp/**/*",
+      ],
+    },
+  },
 };
 
 module.exports = nextConfig;
